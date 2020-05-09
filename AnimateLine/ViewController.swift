0257fc2f32f5074be9dba2ea7fdab3b5f796801e
AsahiOcean <sergfedotov@icloud.com>
2020-05-09 08:48:29 +0300

    Initial Commit

---
+++ AnimateLine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimateLine
+//
+//  Created by Serg Fedotov on 09.05.2020.
+//  Copyright © 2020 Sergey Fedotov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
